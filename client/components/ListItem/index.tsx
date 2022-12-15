508580318e26e29a09a3a0946abb0b58e9e2a043
enes-erenn <thenesern@gmail.com>
2022-12-15 21:22:32 +0300

    List Item improvements

--- client/components/ListItem/index.tsx
+++ client/components/ListItem/index.tsx
@@ -7,10 +7,18 @@ interface Props {
 }
 
 const ListItem: React.FC<Props> = ({ note }) => {
+  const title = note.body.split("\n")[0].slice(0, 45);
+  const others = note.body.replace(title, "").slice(0, 45);
+  const date = new Date(note.updated).toLocaleDateString();
+
   return (
     <Link href={`/note/${note.id}`}>
       <li>
-        <h3>{note.body}</h3>
+        <h3>{title}</h3>
+        <div>
+          <span>{date}</span>
+          <p>{others} ...</p>
+        </div>
       </li>
     </Link>
   );
--- client/types/types.ts
+++ client/types/types.ts
@@ -1,4 +1,5 @@
 export interface Note {
   body: string;
   id: number;
+  updated: string;
 }
